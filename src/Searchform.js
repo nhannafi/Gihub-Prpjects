d8f96ff78adf0b38a920cc50cf78c26c9dc1d464
nhannafi <nour.hannafi@engie.com>
2020-11-23 09:43:23 +0100

    modify csss

--- src/Searchform.js
+++ src/Searchform.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {faSearch} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import logo from './logo.jpg';
 
 const Searchform = ({ handleSubmit}) => (
